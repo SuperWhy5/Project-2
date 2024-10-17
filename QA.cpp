651853bb8177c5fa9befc2e96c9185613622b4d9
Javien <javienme@gmail.com>
2024-10-17 18:48:21 -0400

    lettercount

--- QA.cpp
+++ QA.cpp
@@ -14,12 +14,25 @@ std::string QA::getAnswer(){
 }
 
 int QA::getScore(){
-
+    return score;
 }
 
 int QA::letterCount(){
 
-    return 0;
+    int count = 0;
+
+    for(char i : question){
+        if(std::isalpha(i)){
+            count++;
+        }
+    }
+    for(char i : answer){
+        if(std::isalpha(i)){
+            count++;
+        }
+    }
+
+    return count;
 }
 
 void QA::updateScore(int val){
